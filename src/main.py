2c9e98177dfea326570ec9623ae080d0d3fa5788
uros <uros.isakovic98@gmail.com>
2020-03-23 23:05:55 +0100

    Created automated appearence of simulated news data in home page.

--- src/main.py
+++ src/main.py
@@ -3,11 +3,24 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = ''
 
+#TODO: Use some date format insted of the string.
+news = [
+    {
+        'news_title': 'Lorem1 title1',
+        'content': 'lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1',
+        'date': '24-02-2020'
+    },
+    {
+        'news_title': 'Lorem2 title2',
+        'content': 'lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2',
+        'date': '28-02-2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', newslist=news)
 
 
 @app.route("/results")
