69a04de1fe5770706e6f0e4da9dcc1a36ab0bf58
uros <uros.isakovic98@gmail.com>
2020-03-23 23:38:29 +0100

    Separated all artificial data in dummydata.py file. Added some TODOs.

---
+++ src/dummydata.py
@@ -0,0 +1,13 @@
+#TODO: Use some date format insted of the string.
+news = [
+    {
+        'news_title': 'Lorem1 title1',
+        'content': 'lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1',
+        'date': '24-02-2020'
+    },
+    {
+        'news_title': 'Lorem2 title2',
+        'content': 'lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2',
+        'date': '28-02-2020'
+    }
+]
--- src/main.py
+++ src/main.py
@@ -2,26 +2,15 @@ from flask import Flask, render_template, flash, redirect, url_for
 from forms import LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '1234'
+import dummydata
 
-#TODO: Use some date format insted of the string.
-news = [
-    {
-        'news_title': 'Lorem1 title1',
-        'content': 'lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1 lorem1 ipsum1',
-        'date': '24-02-2020'
-    },
-    {
-        'news_title': 'Lorem2 title2',
-        'content': 'lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2 lorem2 ipsum2',
-        'date': '28-02-2020'
-    }
-]
+#TODO: Generate real random key
+app.config['SECRET_KEY'] = '1234'
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', newslist=news)
+    return render_template('home.html', newslist=dummydata.news)
 
 
 @app.route("/results")
