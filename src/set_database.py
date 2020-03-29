c7d7dc78511b3f1bd87a048a1e8f1059b83b587a
uros <uros.isakovic98@gmail.com>
2020-03-29 14:43:19 +0200

    Added a scripy witch filles the db with artificial data

--- src/set_database.py
+++ src/set_database.py
@@ -1,69 +1,117 @@
 import os
 from datetime import datetime
+from random import randint
 from fuca import db
 from fuca.models import Match, News, Player, Statistics, Team
 
-# remove site.db
-if os.path.exists('fuca/site.db'):
-    print("Removed site.db")
-    os.remove('fuca/site.db')
-
-# create db
-db.create_all()
-
-# add news
-news = News(title='t1', content='c1')
-print(news)
-
-db.session.add(news)
-db.session.commit()
-
-print(News.query.all())
-
-# add teams
-team1 = Team(name='rekreativo')
-team2 = Team(name='ynwa')
-
-db.session.add(team1)
-db.session.add(team2)
-db.session.commit()
-
-print(Team.query.all())
-
-# add players
-player = Player(name='viki',
-                birthdate=datetime.now(),
-                image='default.jpg',
-                email='viki@gmail.com',
-                password='uros',
-                team_id=1)
-print(player)
-
-db.session.add(player)
-db.session.commit()
-
-print(Player.query.all())
-
-# add matches
-match = Match(host_team=team1,
-              guest_team=team2)
-
-db.session.add(match)
-db.session.commit()
-
-print(Match.query.all())
+# configu
+NEWS_CNT = 6
+TEAMS_CNT = 6
+PLAYER_CNT = 30
+assert PLAYER_CNT % TEAMS_CNT == 0, 'Every team must have the same number of players'
 
 
-# add statistics
-stats = Statistics(goals=5,
-                   assists=2,
-                   experience=100,
-                   yellow=1,
-                   red=0,
-                   player_id=1,
-                   match_id=1)
+def print_array(arr, newlines=3):
+    for el in arr:
+        print(el)
+    for nl in range(newlines):
+        print('\n')
 
-db.session.add(stats)
-db.session.commit()
 
-print(Statistics.query.all())
+def init_empty_db():
+    if os.path.exists('fuca/site.db'):
+        print("Removed site.db")
+        os.remove('fuca/site.db')
+
+    db.create_all()
+
+
+def add_news():
+    news_titles = ['title_' + str(i) for i in range(NEWS_CNT)]
+    news_contents = ['content_' + str(i) for i in range(NEWS_CNT)]
+    news_arr = [News(title=news_titles[i], content=news_contents[i]) for i in range(NEWS_CNT)]
+
+    for news in news_arr:
+        db.session.add(news)
+    db.session.commit()
+
+    print_array(News.query.all())
+
+
+# TODO: Add captain foreign key
+def add_teams():
+    team_names = ['team_name_' + str(i) for i in range(TEAMS_CNT)]
+    teams = [Team(name=team_names[i]) for i in range(TEAMS_CNT)]
+
+    for team in teams:
+        db.session.add(team)
+    db.session.commit()
+
+    print_array(Team.query.all())
+
+
+def add_players():
+    player_names = ['player_name' + str(i) for i in range(PLAYER_CNT)]
+    player_birthdates = [datetime.now() for _ in range(PLAYER_CNT)]
+    player_images = ['image' + str(i) + '.png' for i in range(PLAYER_CNT)]
+    player_emails = ['player' + str(i) + '@gmail.com' for i in range(PLAYER_CNT)]
+    player_passwords = ['password' + str(i) for i in range(PLAYER_CNT)]
+    player_team_ids = [i % (PLAYER_CNT / TEAMS_CNT) + 1 for i in range(PLAYER_CNT)]
+
+    players = [Player(name=player_names[i],
+                     birthdate=player_birthdates[i],
+                     image=player_images[i],
+                     email=player_emails[i],
+                     password=player_passwords[i],
+                     team_id = player_team_ids[i]) for i in range(PLAYER_CNT)]
+
+    for player in players:
+        db.session.add(player)
+    db.session.commit()
+
+    print_array(Player.query.all())
+
+
+def add_matches():
+    teams = Team.query.all()
+    for host_team in teams:
+        for guest_team in teams:
+            if host_team != guest_team:
+                match = Match(host_team=host_team, guest_team=guest_team)
+                db.session.add(match)
+    
+    db.session.commit()
+    print_array(Match.query.all())
+
+
+def add_statistics():
+    players = Player.query.all()
+    matches = Match.query.all()
+
+    for player in players:
+        for match in matches:
+            if player.team in [match.host_team, match.guest_team]:
+                stats = Statistics(goals=randint(0, 5),
+                                   assists=randint(0, 5),
+                                   experience=randint(0, 100),
+                                   yellow=randint(0, 2),
+                                   red=randint(0, 2),
+                                   player=player,
+                                   match=match)
+                db.session.add(stats)
+
+    db.session.commit()
+    print_array(Statistics.query.all())
+
+
+def main():
+    init_empty_db()
+    add_news()
+    add_teams()
+    add_players()
+    add_matches()
+    add_statistics()
+
+
+if __name__ == '__main__':
+    main()
