6da41855ba4ec29f3d5b854a1bc66eaae88668e4
uros <uros.isakovic98@gmail.com>
2020-05-09 11:41:13 +0200

    Reformatted __init__.py

--- src/fuca/__init__.py
+++ src/fuca/__init__.py
@@ -14,55 +14,38 @@ login_manager.login_message_category = 'info'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    # import all the configurations
     app.config.from_object(Config)
 
-    # register users blueprint
+    # import blueprints
     from fuca.main.routes import main
-    app.register_blueprint(main)
-
-    # register users blueprint
     from fuca.users.routes import users
-    app.register_blueprint(users)
-
-    # register teams blueprint
     from fuca.teams.routes import teams
-    app.register_blueprint(teams, url_prefix='/teams')
-
-    # register standings blueprint
     from fuca.standings.routes import scores
-    app.register_blueprint(scores, url_prefix='/standings')
-
-    # register admin blueprint
     from fuca.admin.news.routes import news
-    app.register_blueprint(news, url_prefix='/admin/news')
-
-    # register admin blueprint
     from fuca.admin.teams.routes import adminteams
-    app.register_blueprint(adminteams, url_prefix='/admin/teams')
-
-    # register players blueprint
     from fuca.admin.players.routes import players
-    app.register_blueprint(players, url_prefix='/admin/players')
-
-    # register matches blueprint
     from fuca.admin.matches.routes import matches
-    app.register_blueprint(matches, url_prefix='/admin/matches')
-
-    # register results blueprint
     from fuca.admin.results.routes import results
-    app.register_blueprint(results, url_prefix='/admin/results')
-
-    # register statistics blueprint
     from fuca.admin.statistics.routes import statistics
-    app.register_blueprint(statistics, url_prefix='/admin/statistics')
-
-    # register statistics blueprint
     from fuca.admin.routes import adminhome
-    app.register_blueprint(adminhome, url_prefix='/admin')
-
     from fuca.errors.handlers import errors
+
+    # register blueprints
+    app.register_blueprint(main)    
+    app.register_blueprint(users)    
+    app.register_blueprint(teams,       url_prefix='/teams')
+    app.register_blueprint(scores,      url_prefix='/standings')
+    app.register_blueprint(adminhome,   url_prefix='/admin')
+    app.register_blueprint(news,        url_prefix='/admin/news')
+    app.register_blueprint(adminteams,  url_prefix='/admin/teams')
+    app.register_blueprint(players,     url_prefix='/admin/players')
+    app.register_blueprint(matches,     url_prefix='/admin/matches')
+    app.register_blueprint(results,     url_prefix='/admin/results')
+    app.register_blueprint(statistics,  url_prefix='/admin/statistics')
     app.register_blueprint(errors)
 
+    # initiate flask extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
