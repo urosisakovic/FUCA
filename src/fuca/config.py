be0615b8e99c619c70935d02ae077bf1b75798ed
Uros Isakovic <uros.isakovic98@gmail.com>
2020-05-18 20:54:36 +0200

    Added favicon and a real secret key

--- src/fuca/config.py
+++ src/fuca/config.py
@@ -1,4 +1,4 @@
 class Config:
     #TODO: Generate real random key
-    SECRET_KEY = '1234'
+    SECRET_KEY = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTU4OTgyNzc5OCwiZXhwIjoxNTg5ODI3ODI4fQ.eyJ1c2VyX2lkIjoxfQ.nJIyRLp3_5dnhEgVIveCZ33534UztJ0_ZnbY0_BjJ4XM3mxxO8zOF5vMT3Nxo6lAE0LOk2vWDrUeUm9rl0HNDw'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
